8fa6b95156c08f9b8e8c5052ab5f439a65d9c5d2
VuCongThanh96 <101028090+VuCongThanh96@users.noreply.github.com>
2022-03-07 23:07:32 +0700

    Update Kiểm tra số chẵn lẻ.py

--- Kiểm tra số chẵn lẻ.py
+++ Kiểm tra số chẵn lẻ.py
@@ -1,5 +1,4 @@
 # Chương trình kiểm tra số chẵn lẻ bằng toán tử ba ngôi
-
 print('Đây là chương trình kiểm tra số nguyên chẵn hay lẻ')
 a = int(input('Nhập vào số bạn muốn kiểm tra: '))
 b = 'số chẵn' if (a % 2 == 0) else 'số lẻ'
