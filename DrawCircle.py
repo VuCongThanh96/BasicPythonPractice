a6d0798c9186eb25621fdcd3f28b786998d696c3
VuCongThanh96 <101028090+VuCongThanh96@users.noreply.github.com>
2022-03-09 18:06:58 +0700

    Add files via upload

---
+++ DrawCircle.py
@@ -0,0 +1,23 @@
+import turtle
+t = turtle.Turtle()
+# Vẽ 1 hình tròn đơn giản
+t.circle(50)
+# Đổi màu cho pen
+t.pencolor("blue")
+# Vẽ và tô màu bên trong hình tròn tại 1 điểm tọa độ mới
+t.goto(150, 100)
+turtle.fillcolor("yellow")
+turtle.begin_fill()
+turtle.circle(25)
+turtle.end_fill()
+# Thay đổi kích thước pen
+t.right(90)
+t.forward(250)
+t.pensize(7)
+for i in range(4):
+    t.left(90)
+    t.forward(150)
+# Vẽ một chấm tròn
+t.goto(-25, -230)
+t.dot(150)
+turtle.done()
