d5526596da130805329999dd0b6834eceb50f342
VuCongThanh96 <101028090+VuCongThanh96@users.noreply.github.com>
2022-03-13 10:10:54 +0700

    Update In khoảng a-b.py

--- In khoảng a-b.py
+++ In khoảng a-b.py
@@ -1,12 +1,13 @@
-print('Đây là chương trình in ra dãy số thuộc [a;b]')
+print('Đây là chương trình in ra dãy số thuộc -1000<=[a;b]<=1000 (a,b đều là số nguyên tùy bạn chọn giá trị)')
 a = int(input('Nhập vào số bắt đầu:'))
 b = int(input('Nhập vào số kết thúc:'))
 if a > b or a < -1000 or b > 1000:
     print('Bạn đã nhập sai, hãy nhập lại')
-x = a
-print('Dãy số là:')
-while x in range(a, b+1):
-    print(x, end='. ')
-    x = x + 1
-    if x > b:
-        break
+else:
+    x = a
+    print('Dãy số là:')
+    for x in range(a, b+1):
+        print(x, end='. ')
+        x = x + 1
+        if x > b:
+            break
